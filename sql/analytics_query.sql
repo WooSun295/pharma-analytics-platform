493de47255f70ade4f103c9f51efa36f18273939
Sunwoo Lee <slee2021@fau.edu>
2026-01-28 20:47:16 -0500

    Created materialized view using the ETL data, then changed analytics_query.sql to query from the MV

--- sql/analytics_query.sql
+++ sql/analytics_query.sql
@@ -1,16 +1,9 @@
 SELECT
-    p.state,
-    p.provider_type,
-    d.drug_name,
-    d.generic_name,
-    SUM(f.sales_amount) AS sales_amount,
-    SUM(f.total_claims) AS total_claims
-FROM fact_sales f
-JOIN dim_provider p ON f.provider_id = p.provider_id
-JOIN dim_drug d ON f.drug_id = d.drug_id
-WHERE f.sale_year = %(sale_year)s
-GROUP BY 
-    p.state, 
-    p.provider_type, 
-    d.drug_name,
-    d.generic_name;
+  state,
+  provider_type,
+  drug_name,
+  generic_name,
+  sales_amount,
+  total_claims
+FROM mv_sales_agg_y2023
+WHERE sale_year = %(sale_year)s;
